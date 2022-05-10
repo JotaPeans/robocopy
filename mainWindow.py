82bb4cab1dfd81b9e2e2b4a9528909c3d3d3d111
JotaPeans <jpfontesferreira1@gmail.com>
2022-05-10 00:40:27 -0300

    update

--- mainWindow.py
+++ mainWindow.py
@@ -6,9 +6,17 @@ mainWindow = QtWidgets.QApplication([])
 
 class Ui_MainWindow:
     def __init__(self) -> None:
+        self.timeframe = 5
         self.mainForm = uic.loadUi('ui/MainWindow.ui')
-        self.mainForm.pushButton.clicked.connect(self.config)
         self.corrigirSinais()
+
+        # Set Home Page
+        self.mainForm.stackedWidget.setCurrentIndex(0)
+        # Home Page!
+        self.mainForm.HomeButton.clicked.connect(self.homePage)
+        # Settings Page!
+        self.mainForm.SettingsButton.clicked.connect(self.settingsPage)
+
         self.mainForm.show()
 
     def corrigirSinais(self):
@@ -25,10 +33,17 @@ class Ui_MainWindow:
             dados_1 = dados[1].replace(' ', '')
             dados_2 = dados[2].replace(' ', '')
             dados_2 = dados_2.lower()
-            sinais = f'{dados[0]}:00,{dados_1},{dados_2},{5}'
-            print(sinais)
+            sinais = f'{dados[0]}:00,{dados_1},{dados_2},{self.timeframe}'
             with open(f'sinais corrigidos.txt', 'a') as file:
                 file.write(f'{sinais}\n')
+
+    def homePage(self):
+        self.mainForm.stackedWidget.setCurrentIndex(0)
+        self.mainForm.currentWindow.setText('HOME')
+
+    def settingsPage(self):
+        self.mainForm.stackedWidget.setCurrentIndex(1)
+        self.mainForm.currentWindow.setText('SETTINGS')
 
     def config(self):
         pass
