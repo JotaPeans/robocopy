5d0e5ea81d2de27bd8af52198e7f34da626bdef0
JotaPeans <jpfontesferreira1@gmail.com>
2022-05-11 15:30:16 -0300

    update  BotWindow

--- BotWindow.py
+++ BotWindow.py
@@ -1,7 +1,10 @@
+from time import time
 from PyQt6 import uic, QtWidgets
 import sys
 import os
 from datetime import datetime, timedelta
+
+from cv2 import exp
 
 mainWindow = QtWidgets.QApplication([])
 
@@ -12,6 +15,7 @@ class Ui_MainWindow:
         self.timeframe = 5
         self.mainForm = uic.loadUi('ui/MainWindow.ui')
         self.mainForm.tableWidget.setColumnWidth(5, 104)
+        self.mainForm.tableWidget.setColumnWidth(1, 76)
         #self.corrigirSinais()
 
         # Set Home Page
@@ -39,32 +43,36 @@ class Ui_MainWindow:
 
         row = 0
         for x in lista:
-            dados = x.split(' ')
-            dados_1 = dados[1].replace(' ', '')
-            dados_2 = dados[2].replace(' ', '')
-            dados_2 = dados_2.lower()
-            sinais = f'{dados[0]}:00,{dados_1},{dados_2},{self.timeframe}'
-
-            #print(self.mainForm.tableWidget.row())
-
-            self.mainForm.tableWidget.setRowCount(len(lista))
-            #table date value
-            self.mainForm.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(f'{self.dateToday}'))
-            #table hour value
-            self.mainForm.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(f'{dados[0]}:00'))
-            #table par value
-            self.mainForm.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(dados_1))
-            #table dir value
-            self.mainForm.tableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem(dados_2.upper()))
-            #table timeframe value
-            self.mainForm.tableWidget.setItem(row, 4, QtWidgets.QTableWidgetItem(f'{self.timeframe}'))
-            #table Status value
-            self.mainForm.tableWidget.setItem(row, 5, QtWidgets.QTableWidgetItem('Em espera...'))
-
-            row += 1
-
-            with open(f'sinais corrigidos.txt', 'a') as file:
-                file.write(f'{sinais}\n')
+            if lista[0] == 'Mestre dos sinais VIP':
+                try:
+                    dados = x.split(', ')
+                    horario = f'{dados[0]}:00'
+                    par = dados[1].replace(' ', '')
+                    timeframe = dados[2].replace('M1', '1')
+                    timeframe = timeframe.replace('M5', '5')
+                    timeframe = timeframe.replace('M15', '15')
+                    dir = dados[3].replace(' ', '')
+                    dir = dir.lower()
+                    sinal = f'{horario},{par},{dir},{timeframe}'
+                    self.showTable(lista=lista, row=row, horario=horario, par=par, dir=dir, timeframe=timeframe)
+                    self.writeFile(sinal=sinal)
+                    row += 1
+                except:
+                    print('erro de formatação')
+            
+            elif lista[0] == 'Tigre dos Sinais':
+                try:
+                    dados = x.split(' ')
+                    horario = f'{dados[0]}:00'
+                    par = dados[1].replace(' ', '')
+                    dir = dados[3].replace(' ', '')
+                    dir = dir.lower()
+                    sinal = f'{horario},{par},{dir},{self.timeframe}'
+                    self.showTable(lista=lista, row=row, horario=horario, par=par, dir=dir, timeframe=str(self.timeframe))
+                    self.writeFile(sinal=sinal)
+                    row += 1
+                except:
+                    print('erro de formatação')
 
     def homePage(self):
         self.mainForm.stackedWidget.setCurrentIndex(0)
@@ -74,5 +82,21 @@ class Ui_MainWindow:
         self.mainForm.stackedWidget.setCurrentIndex(1)
         self.mainForm.currentWindow.setText('SETTINGS')
 
-    def config(self):
-        pass
+    def showTable(self, lista:list, row:int, horario:str, par:str, dir:str, timeframe:str):
+        self.mainForm.tableWidget.setRowCount(len(lista))
+        #table date value
+        self.mainForm.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(f'{self.dateToday}'))
+        #table hour value
+        self.mainForm.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(horario))
+        #table par value
+        self.mainForm.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(par))
+        #table dir value
+        self.mainForm.tableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem(dir.upper()))
+        #table timeframe value
+        self.mainForm.tableWidget.setItem(row, 4, QtWidgets.QTableWidgetItem(timeframe))
+        #table Status value
+        self.mainForm.tableWidget.setItem(row, 5, QtWidgets.QTableWidgetItem('Em espera...'))
+
+    def writeFile(self, sinal:str):
+        with open(f'sinais corrigidos.txt', 'a') as file:
+            file.write(f'{sinal}\n')
